c49c1e8370927055c1669043f4e3bc4250c243c0
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-31 18:02:12 -0300

    Atualizações do projeto.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
@@ -17,6 +23,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -31,4 +38,25 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+}
+
+createCactus()
 document.addEventListener('keyup', handleKeyUp);
